59209678847f0a4fe2cd0c04d7115b433b12ed09
HR-XXII <betazona1984@gmail.com>
2026-06-07 01:42:00 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+// firebase/config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB39WPXW-vV25QQiUB5Fkk9di4cUVfpCjo",
   authDomain: "albimo-shopee.firebaseapp.com",
+  databaseURL: "https://albimo-shopee-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "albimo-shopee",
   storageBucket: "albimo-shopee.firebasestorage.app",
   messagingSenderId: "349404045321",
   appId: "1:349404045321:web:6d5b49cd1897d4e51418d8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.database();
+
+export const cloudinaryConfig = {
+    cloudName: "diutiwqz1",
+    uploadPreset: "albimostore"
+};
+
 };
 
 const app = initializeApp(firebaseConfig);
